7b9e6bb9acba70f6833d03532875796b2ba626bc
AnastasiaStadnik7 <5603119@list.ru>
2023-06-21 17:08:29 +0300

    accepts 21.06

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  defaultCommandTimeout: 8000,
   e2e: {
+    baseUrl: 'https://itle.buzz/kazan',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
